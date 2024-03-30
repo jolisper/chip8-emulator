e6290a38923e6502a0004f77ef0c8e151b74e9ee
Jorge Luis Perez <jolisper@protonmail.com>
2024-03-30 18:03:43 +0000

    Some code cleaning

--- chip8-avsys/src/lib.rs
+++ chip8-avsys/src/lib.rs
@@ -77,22 +77,7 @@ impl AudioCallback for SquareWave {
     }
 }
 
-// #[derive(Parser)]
-// struct Args {
-//     rom_file: String,
-//     debug: bool,
-// }
-
 pub fn start(rom: Vec<u8>, debug_mode: bool) -> Result<(), String> {
-    // let args = Args::parse();
-    // let rom_file_name = args.rom_file;
-    // let debug_mode = args.debug;
-
-    // // Load ROM file
-    // let mut file = std::fs::File::open(rom_file_name).unwrap();
-    // let mut buf = Vec::new();
-    // file.read_to_end(&mut buf).expect("read all ROM file");
-
     let mut chip8: VM = VM::new();
     chip8.load_program(&rom)?;
 
